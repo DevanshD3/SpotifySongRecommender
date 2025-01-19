557f768cacfac27a2439119a193fc5cd186a5778
devanshdixit <f20190069@goa.bits-pilani.ac.in>
2025-01-20 00:02:42 +0530

    First Push

---
+++ src/test/kotlin/com/dev/project/spotify/SpotifyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dev.project.spotify
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpotifyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
